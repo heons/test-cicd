4802f5bd7836cb60dc02be7eb296109acd0af007
Huen Oh <hoh@lightboxre.com>
2023-07-04 11:16:18 -0400

    Revert "fix(scope): test publish without latest tag"
    This reverts commit 006e5a7a84f5c60a30275f166e2cd741ee8b6658.

--- scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove the "latest" tag from package.json
-jq 'del(.distTags.latest)' package.json > package-temp.json
-mv package-temp.json package.json
-
-# Run semantic-release
-npx semantic-release
